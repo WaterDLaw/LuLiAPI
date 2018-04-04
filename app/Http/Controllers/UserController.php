7cb26fd72496f38dfc621d35d939b2a8be6a85e5
WaterDLaw <danytlaw.dev@gmail.com>
2018-04-04 08:24:41 +0200

    all

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use JWTFactory;
+use JWTAuth;
+use Validator;
+use Response;
+
+class UserController extends Controller{
+
+    // Function zum anmelden des Benutzers
+    public function register(Request $request){
+
+        // Der Validator überprüft ob die Daten des request alle in Ordnung und vorhanden sind
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255|unique:users',
+            'name' => 'required',
+            'password'=> 'required'
+        ]);
+
+        // Falls die Daten nicht ok sind so sende die Errors zurück
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        // Kreier den User und speichere in
+        User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password')),
+        ]);
+        // Nehme den neusten User (first) der gerade kreiert wurde und erstelle ein JWT-Token
+        $user = User::first();
+        $token = JWTAuth::fromUser($user);
+        
+        return Response::json(compact('token'));
+    }
+
+    // Function welche sich um das Login der Benutzer kümmert
+    public function login(Request $request){
+
+
+        // Der Validator überprüft ob die Daten des request alle in Ordnung und vorhanden sind
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password'=> 'required'
+        ]);
+
+        // Falls die Daten nicht vorhanden oder falsch formatiert sind returne die Errors
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        // Erstelle die Credentials mit den eingegeben Daten
+        $credentials = $request->only('email', 'password');
+
+        /**
+         * Schritte um das Login korrekt zu überprüfen
+         * 
+         * try: Versuche den User einzuloggen und erstelle Token
+         *      Falls dies nicht möglich ist waren es falsche credentials
+         * 
+         * catch: Fange alle Fehler ab welche mit der JWT Auth passieren könnten
+         * 
+         * Falls alles geklappt hat return JWT Token
+         */
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+        
+        return response()->json(compact('token'));
+    }
+}
