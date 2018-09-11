f6c3512e26c2c9a4c4950197e4978767424bca99
WaterDLaw <danytlaw.dev@gmail.com>
2018-09-11 22:00:42 +0200

    test script

---
+++ storage/app/public/pdf/pdf.php
@@ -0,0 +1,48 @@
+<?php
+use mikehaertl\pdftk\Pdf;
+use mikehaertl\pdftk\XfdfFile;
+use mikehaertl\pdftk\FdfFile;
+/*
+// Fill form with data array
+//$path = storage_path('app/public/pdf/my_converted.pdf');
+//info($path);
+$pdf = new Pdf('/home/danytlaw/Downloads/my_converted.pdf');
+
+$xfdf = new XfdfFile(['Text9' => 'Daniel']);
+$xfdf->saveAs('/home/danytlaw/Downloads/data.xfdf');
+
+$pdf->fillForm('/home/danytlaw/Downloads/data.xfdf');
+//$pdf->flatten();      
+
+$pdf->send("Test");
+
+ /*    
+ if (!$pdf->saveAs('/home/danytlaw/Downloads/filled.pdf')) {
+     $error = $pdf->getError();
+     info($error);
+ }
+ */
+
+$pdf = new Pdf('./my_converted.pdf', [
+    'command' => '/app/vendor/pdftk/bin/pdftk',
+	'useExec' => true
+]);
+
+//$data = $pdf->getDataFields();
+
+// Get data as string
+//echo $data;
+
+
+$pdf->fillForm([
+        'Text9' => 'Herzog',
+        'Text10' => 'Daniel'
+    ])
+->needAppearances();
+
+// Check for errors
+if (!$pdf->saveAs('filled.pdf')) {
+    $error = $pdf->getError();
+	echo $error;
+}
+?>
