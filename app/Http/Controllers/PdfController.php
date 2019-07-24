0daf49f207f01a2a5bbc63c2a219fc45ba910e55
WaterDLaw <danytlaw.dev@gmail.com>
2019-07-24 13:22:19 +0100

    fehler frontend nochmals back testen

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -74,14 +74,15 @@ class PdfController extends Controller
             $error = $pdf->getError();
             Log::error ("Could not save pdf:" . $error);
         }
-        Storage::delete('temp_signature_pdf/temp_signature.pdf');
+        Storage::delete('temp_signature.pdf');
         return response()->download('/app/storage/app/public/pdf/filleder.pdf');
     }
 
     public function uploadTempSignature(Request $request)
     {
         // Store signature pdf in Storage
-        $path = $request->file('signature')->store('test');
+        $path = $request->file('signature')->store('temp_signature_pdf', "tester.pdf");
+   
         
 
     }
