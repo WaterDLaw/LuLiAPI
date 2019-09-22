e94f3f35f9bea61c0d1e23106250ca04e7d3d9b3
WaterDLaw <danytlaw.dev@gmail.com>
2019-09-22 10:52:06 +0100

    Spirotmetrie data testing

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -141,8 +141,24 @@ class PdfController extends Controller
         $groesse = $patient->messwerte->groesse_vor;
         $gewicht_vor = $patient->messwerte->gewicht_vor;
         $gewicht_nach = $patient->messwerte->gewicht_nach;
+        $bmi_vor = $patient->messwerte->bmi_vor;
+        $bmi_nach = $patient->messwerte->bmi_nach;
 
         // Spirometrie
+        $fev1l_vor = $patient->messwerte->fev1l_vor;
+        $fev1l_nach = $patient->messwerte->fev1l_nach
+        $fev1soll_vor = $patient->messwerte->fev1soll_vor;
+        $fev1soll_nach = $patient->messwerte->fev1soll_nach;
+        $fvc_vor = $patient->messwerte->fvc_vor;
+        $fvc_nach = $patient->messwerte->fvc_nach;
+        $rv_vor = $patient->messwerte->rv_vor;
+        $rv_nach = $patient->messwerte->rv_nach;
+        $tlc_vor = $patient->messwerte->tlc_vor;
+        $tlc_nach = $patient->messwerte->tlc_nach;
+        $fev1_fvc_vor = $patient->messwerte->fev1_fvc_vor;
+        $fev1_fvc_nach = $patient->messwerte->fev1_fvc_nach;
+        $rv_tlc_vor = $patient->messwerte->rv_tlc_vor;
+        $rv_tlc_nach = $patient->messwerte->rv_tlc_nach;
 
         // Fill the pdf form
         $pdf->fillForm([
@@ -154,7 +170,19 @@ class PdfController extends Controller
             'Kursnr' => $kurs,
             'VORGrösse m' => $groesse,
             'VORGewicht kg' => $gewicht_vor,
-            'NACHGewicht kg' => $gewicht_nach
+            'NACHGewicht kg' => $gewicht_nach,
+            'VORBMI kgm2' => $bmi_vor,
+            'NACHBMI kgm2' => $bmi_nach,
+            'VORFEV1 l' => $fev1l_vor,
+            'NACHFEV1 l' => $fev1l_nach,
+            'VORFEV1 Soll' => $fev1soll_vor,
+            'NACHFEV1 Soll' => $fev1soll_nach,
+            'VORFVC l' => $fvc_vor,
+            'NACHFVC l' => $fvc_nach,
+            'VORFEV1FVC' => $fev1_fvc_vor,
+            'NACHFEV1FVC' => $fev1_fvc_nach,
+            'VORRVTLC' => $rv_tlc_vor,
+            'NACHRVTLC' => $rv_tlc_nach
         ])
         ->needAppearances();
 
